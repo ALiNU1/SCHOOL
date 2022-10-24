a6fdd23e8d19d499e6118ad02964e0fa5d65cb76
alinu1 <uwu1343@gmail.com>
2022-10-24 23:29:32 +0600

    first models and func

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Setting
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    setting = Setting.objects.latest('id')
+    context = {
+        'setting' : setting,
+    }
+    return render(request, 'index.html', context)

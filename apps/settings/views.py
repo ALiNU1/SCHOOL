f30b36c902d1ce479a79852845ef32cf4fb8dfe8
alinu1 <uwu1343@gmail.com>
2022-11-03 15:14:50 +0600

    add teacher

--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from .models import Setting
 from apps.main.models import News
+from apps.teachers.models import Teachers
 
 # Create your views here.
 def index(request):
     setting = Setting.objects.latest('id')
     news = News.objects.all()
+    teachers = Teachers.objects.all()
     context = {
         'setting' : setting,
         'news' : news,
+        'teachers' : teachers,
     }
     return render(request, 'schooll/index1.html', context)
--- apps/teachers/models.py
+++ apps/teachers/models.py
@@ -1,3 +1,15 @@
+from random import choices
 from django.db import models
 
 # Create your models here.
+class Teachers(models.Model):
+    teachers_jobtitle = models.CharField(max_length = 100)
+    photo = models.ImageField(upload_to = 'teachers/')
+    name = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Учитель'
+        verbose_name_plural = 'Учителя'
